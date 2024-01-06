bab15a0d34f4becb92d40a7998946e05dc48fa4b
ozlem <06ozlemozturk@gmail.com>
2024-01-06 21:36:43 +0300

    next

--- middlewares.js
+++ middlewares.js
@@ -13,5 +13,20 @@ const PORT = process.env.PORT || 8000;
 //? Middleware functions must be has three parameters.
 //? Last parameter is for next().
 
+//next() for next Function
+
+app.get("/", (req, res, next) => {
+  next();
+  res.send({
+    message: "Middleware running",
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send({
+    message: " Welcome",
+  });
+});
+
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
